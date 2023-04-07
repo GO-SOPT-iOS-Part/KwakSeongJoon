ab3b86960a4b972787b4656f91ecd3f9341dde57
Kwak Seong Joon <seongjoonkwak@inu.ac.kr>
2023-04-07 10:31:15 +0900

    Add Project setting

---
+++ SOPT Practice/1st_seminar/first_seminar_practice/first_seminar_practice/SceneDelegate.swift
@@ -0,0 +1,27 @@
+//
+//  SceneDelegate.swift
+//  32th-iOS-Seminar
+//
+//  Created by 김승찬 on 2023/01/14.
+//
+
+import UIKit
+
+class SceneDelegate: UIResponder, UIWindowSceneDelegate {
+    
+    var window: UIWindow?
+    
+    func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
+        
+        // 1.
+        guard let windowScene = (scene as? UIWindowScene) else { return }
+    // 2.
+        let navigationController = UINavigationController(rootViewController: FirstViewController_1st_Seminar())
+        self.window = UIWindow(windowScene: windowScene)
+    // 3.
+    
+        self.window?.rootViewController = navigationController
+    // 4.
+        self.window?.makeKeyAndVisible()
+    }
+}
